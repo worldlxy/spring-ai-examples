69e69492513b2d3d53d98809646343cf18e43436
Christian Tzolov <christian.tzolov@broadcom.com>
2025-01-31 11:24:15 +0100

    Clean code

--- model-context-protocol/mcp-webflux-server/src/main/java/org/springframework/ai/mcp/sample/server/McpServerApplication.java
+++ model-context-protocol/mcp-webflux-server/src/main/java/org/springframework/ai/mcp/sample/server/McpServerApplication.java
@@ -7,31 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class McpServerApplication {
 
 	public static void main(String[] args) {
-
-		// var stackThread = new Thread() {
-		// @Override
-		// public void run() {
-		// try {
-		// while (true) {
-		// Thread.sleep(5000);
-		// java.util.Collection<java.lang.StackTraceElement[]> a1 =
-		// java.lang.Thread.getAllStackTraces()
-		// .values();
-		// for (java.lang.StackTraceElement[] a2 : a1) {
-		// System.out.println("==========");
-		// for (java.lang.StackTraceElement a3 : a2) {
-		// System.out.println(a3.toString());
-		// }
-		// }
-		// }
-		// } catch (InterruptedException e) {
-		// e.printStackTrace();
-		// }
-		// }
-		// };
-		// stackThread.setDaemon(false);
-		// stackThread.start();
-
 		SpringApplication.run(McpServerApplication.class, args);
 	}
 
